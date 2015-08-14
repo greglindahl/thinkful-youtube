80bfde14d204b9c18aca232d793c97411e3741e7
greglindahl <greglindahl410@gmail.com>
2015-08-14 09:46:56 -0600

    refractored code to display youtube snippets.  Corrected JSON in params object

--- js/app.js
+++ js/app.js
@@ -8,29 +8,26 @@ $(function(){
 
 function getRequest (searchTerm) {
 	var params = {
-		part: 'snippet'
-		key: 'AIzaSyDhPfCZffmwOxUui-RcXFu3RjjmDnU4BG0'
-		q: 'searchTerm'
+		part: 'snippet',
+		key: 'AIzaSyDhPfCZffmwOxUui-RcXFu3RjjmDnU4BG0',
+		q: searchTerm
 	};
 	url = 'https://www.googleapis.com/youtube/v3/search';
 
-	$.getJSON(url, params, function(data){
-		showResults(data.Search);
-	});
+	$.getJSON(url, params, showResults);
 }
 
 function showResults(results){
   var html = '';
-  $.each(results, function(index,value){
-  	// html += '<p>' + value.Title + '</p>';
-  	console.log(value.Title);
+  $.each(results.items, function(index, item){
+  	html += '<p><img src="'+item.snippet.thumbnails.default.url+'">' + item.snippet.title + '</p>';
+  	console.log(item.snippet.title);
   });
-  // $('#search-results').html(html);
+  $('#search-results').html(html);
 }
 
 
 $(document).ready(function(){
-
 
 
 });
